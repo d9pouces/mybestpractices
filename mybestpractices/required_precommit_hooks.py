1f8c669f0ae0dadaff0e83904079f2d348d54ead
d9pouces <github@19pouces.net>
2022-12-12 13:56:25 +0100

    Initial commit

--- mybestpractices/required_precommit_hooks.py
+++ mybestpractices/required_precommit_hooks.py
@@ -1,4 +1,6 @@
 """Check if required pre-commit hooks are actually checked."""
+import sys
+from collections import defaultdict
 from importlib.resources import open_text
 from typing import Set, Tuple
 
@@ -14,7 +16,7 @@ def required_precommit_hooks():
     with open_text("mybestpractices", "pre-commit-hooks.yaml") as fd:
         expected = load(fd, Loader=Loader)
     expected_hooks: Set[Tuple[str, str]] = set()
-    for repo_data in expected:
+    for repo_data in expected["repos"]:
         repo_url = repo_data["repo"]
         for hook_data in repo_data["hooks"]:
             hook_id = hook_data["id"]
@@ -22,14 +24,26 @@ def required_precommit_hooks():
     with open(".pre-commit-config.yaml") as fd:
         actual = load(fd, Loader=Loader)
     actual_hooks: Set[Tuple[str, str]] = set()
-    for repo_data in actual:
+    for repo_data in actual["repos"]:
         repo_url = repo_data["repo"]
         for hook_data in repo_data["hooks"]:
             hook_id = hook_data["id"]
             actual_hooks.add((repo_url, hook_id))
+    by_repo_url = defaultdict(lambda: [])
     for (repo_url, hook_id) in sorted(expected_hooks):
         if (repo_url, hook_id) in actual_hooks:
             continue
-        c = f"""-   repo: {repo_url}\n    hooks:\n    -   id: {hook_id}\n"""
-        print("Please add the following hook to .pre-commit-config.yaml:")
-        print(c)
+        by_repo_url[repo_url].append(hook_id)
+    if not by_repo_url:
+        return
+    print("Please add the following hooks to .pre-commit-config.yaml and run")
+    print("`pre-commit autoupdate`:")
+    for (repo_url, hook_ids) in sorted(by_repo_url.items()):
+        print(f"""-   repo: {repo_url}\n    rev: 0.0.0\n    hooks:""")
+        for hook_id in hook_ids:
+            print(f"    -   id: {hook_id}")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    required_precommit_hooks()
