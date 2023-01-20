354929a6bdc7407b958a02ae1ff0b1d0922002ba
d9pouces <github@19pouces.net>
2023-01-20 14:52:21 +0100

    default options for new hooks

--- mybestpractices/required_precommit_hooks.py
+++ mybestpractices/required_precommit_hooks.py
@@ -2,7 +2,7 @@
 import sys
 from collections import defaultdict
 from importlib.resources import open_text
-from typing import Set, Tuple
+from typing import Dict, Tuple
 
 try:
     from yaml import CLoader as Loader
@@ -18,30 +18,32 @@ def main():
     with open(".pre-commit-config.yaml") as fd:
         actual_hooks = read_hooks(fd)
     by_repo_url = defaultdict(lambda: [])
-    for (repo_url, hook_id) in sorted(expected_hooks):
+    for (key, hook_data) in sorted(expected_hooks.items()):
+        repo_url, hook_id = key
         if (repo_url, hook_id) in actual_hooks:
             continue
-        by_repo_url[repo_url].append(hook_id)
+        by_repo_url[repo_url].append(hook_data)
     if not by_repo_url:
         return
     print("Please add the following hooks to .pre-commit-config.yaml and run")
     print("`pre-commit autoupdate`:")
-    for (repo_url, hook_ids) in sorted(by_repo_url.items()):
+    for (repo_url, hooks) in sorted(by_repo_url.items()):
         print(f"""-   repo: {repo_url}\n    rev: 0.0.0\n    hooks:""")
-        for hook_id in hook_ids:
-            print(f"    -   id: {hook_id}")
+        for hook_data in hooks:
+            for k, v in hook_data.items():
+                print(f"    -   {k}: {v}")
     sys.exit(1)
 
 
 def read_hooks(fd):
     """Fetch the list of defined hooks."""
     actual = load(fd, Loader=Loader)
-    actual_hooks: Set[Tuple[str, str]] = set()
+    actual_hooks: Dict[Tuple[str, str], Dict] = {}
     for repo_data in actual["repos"]:
         repo_url = repo_data["repo"]
         for hook_data in repo_data["hooks"]:
             hook_id = hook_data["id"]
-            actual_hooks.add((repo_url, hook_id))
+            actual_hooks[(repo_url, hook_id)] = hook_data
     return actual_hooks
 
 
